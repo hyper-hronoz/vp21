573da38c0ffc6ad9f10a5afafea6e6eaa8d69a22
hyper_hronoz <vladilenzia227@mail.ru>
2023-04-11 22:51:20 +0300

    done

--- dz1/main.cpp
+++ dz1/main.cpp
@@ -4,38 +4,48 @@
 
 using namespace std;
 
-float deg2rad(float deg) { return deg * M_PI / 180; }
 
-void drawCircleSegment(sf::RenderWindow &view) {
-  sf::ConvexShape arr;
+float addition = 0;
+float xPosition = 0;
+float xSpeed = 1;
+float direction = 1;
 
+class Segment {
+private:
+  float deg2rad(float deg) { return deg * M_PI / 180; }
 
-  float segmentAngle = deg2rad(180);
-  float radius = 200;
-  sf::Vector2f radiusPos = view.getView().getCenter();
+public:
+  void drawCircleSegment(sf::RenderWindow &view, sf::Vector2f radiusPos, float radius = 100, float userAngle = 90) {
+    view.clear();
 
-  int circlePoints = 1000;
-  arr.setPointCount(circlePoints);
-  float angle = 0;
+    float segmentAngle = deg2rad(userAngle);
 
-  arr.setPoint(0, radiusPos);
+    int circlePoints = 500;
+    float angle = 0;
 
-  for (int i = 0; i < circlePoints; i++) {
-    sf::Vector2f p(cos(angle + (segmentAngle / 2)) * radius,
-                   sin(angle + (segmentAngle / 2)) * radius);
+    sf::ConvexShape arr;
+    arr.setPointCount(circlePoints);
+    arr.setPoint(0, radiusPos);
 
-    p += radiusPos;
-    arr.setPoint(i, p);
+    for (int i = 0; i < circlePoints; i++) {
+      sf::Vector2f p(cos(angle + (addition - segmentAngle / 2)) * radius,
+                     sin(angle + (addition - segmentAngle / 2)) * radius);
 
-    angle += segmentAngle / circlePoints;
+      addition += 0.0001;
+
+      p += radiusPos;
+      arr.setPoint(i, p);
+
+      angle += segmentAngle / circlePoints;
+    }
+
+    arr.setPoint(circlePoints - 1, radiusPos);
+
+    view.draw(arr);
+
+    view.display();
   }
-
-  // arr.setPoint(circlePoints - 1, radiusPos);
-
-  view.draw(arr);
-
-  view.display();
-}
+};
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 800), "sfmltest");
@@ -47,10 +57,18 @@ int main() {
       if (event.type == sf::Event::Closed)
         window.close();
     }
-    drawCircleSegment(window);
-    // window.clear();
-    // window.draw(shape);
-    // window.display();
+    Segment segment;
+    sf::Vector2f position = window.getView().getCenter();
+    float radius = 100;
+    xPosition += xSpeed * direction;
+    position.x += xPosition;
+    if (position.x + radius > 800) {
+        direction = -1; 
+    }
+    if (position.x - radius < 0) {
+        direction = 1;
+    }
+    segment.drawCircleSegment(window, position, radius);
   }
   return 0;
 }
